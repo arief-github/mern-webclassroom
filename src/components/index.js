f6b469aa1d7744225c368a81a2d90f42c59713e2
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2023-09-24 10:38:07 +0700

    installing libraries and add basic folder structure for app

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import {Navbar} from './Navbar.jsx';
+import {Feed} from './Feed.jsx';
+import {SearchFeed} from './SearchFeed.jsx';
+import {VideoDetail} from './VideoDetail.jsx';
+import {ChannelDetail} from './ChannelDetail.jsx';
+
+export {
+    Navbar,
+    Feed,
+    SearchFeed,
+    VideoDetail,
+    ChannelDetail
+}
